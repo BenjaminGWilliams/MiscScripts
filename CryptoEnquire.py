6f04d8d6f64a0c043821bf045e7a3408d3959acf
Benjamin Williams <kiwiben25@gmail.com>
2017-12-22 21:49:20 +1300

    Updated for Python 3.6 and pricemulti to pricemultifull for future

--- CryptoEnquire.py
+++ CryptoEnquire.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-url = 'https://min-api.cryptocompare.com/data/pricemulti'
-params = dict(fsyms='BTC,ETH,DASH,GVT',tsyms='BTC,USD,NZD')    
+url = 'https://min-api.cryptocompare.com/data/pricemultifull'
+params = dict(fsyms='BTC,ETH,DASH,GVT',tsyms='BTC,USD,NZD')
 resp = requests.get(url=url, params=params)
 data = json.loads(resp.text)
 
-for coin,price in sorted(data.iteritems()):
+for coin,price in sorted(data['RAW'].items()):
     string = ""
-    for currency,value in price.iteritems():
-        string += "{}\t{:10.4f}\t".format(currency,value)
-    print "Coin:{}\tPrice: {}".format(coin,string)
+    for currency,value in price.items():
+        string += "{}\t{:10.4f}\t".format(currency,value['PRICE'])
+    print("Coin:{}\tPrice: {}".format(coin,string))
